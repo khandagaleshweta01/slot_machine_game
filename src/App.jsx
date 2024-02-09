f315cc5e238109c3becad1c3ae19a909a3905876
Shweta <khandagaleshweta2002@gmail.com>
2024-02-09 12:48:39 +0530

    commit1

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,6 @@
 import React from "react";
 import "./index.css"; // Import the CSS file
-
-const  SlotM = (props) => {
-    // let x = '😄';
-    // let y = '😄'; 
-    // let z = '😻'; 
-
-    let x = props.x;
-    let y = props.y;
-    let z = props.z;
-    if( ( x===y) && ( y===z ) ){
-        return(
-            <>
-                <div className="slot_inner">
-                    <h1 style={{ textAlign: 'center' }}>
-                        <span>{x}</span>
-                        <span>{y}</span>
-                        <span>{z}</span>
-                    </h1>
-                    <h2 style={{ textAlign: 'center' }}>This is Matching</h2>
-                    <hr/>
-                </div>
-            </>
-        );
-    }
-    else{
-        return(
-        <>
-                <div className="slot_inner">
-                    <h1 style={{ textAlign: 'center' }}>
-                        <span>{x}</span>
-                        <span>{y}</span>
-                        <span>{z}</span>
-                    </h1>
-                    <h2 style={{ textAlign: 'center' }}>This is Not Matching</h2>
-                    <hr/>
-                </div>
-            </>
-        );
-    }
-}
-
+import "./SlotMachine";
 const App = () => {
     return (
         <>
